b342dc47c5b8664722398efce3362186b90d9a79
Neffff <saymek@o2.pl>
2018-01-07 16:03:12 +0100

    added calendar to each doctor and testing events

--- src/components/DoctorInfo.js
+++ src/components/DoctorInfo.js
@@ -1,5 +1,29 @@
 import React, { Component } from 'react';
+import BigCalendar from 'react-big-calendar';
+import moment from 'moment';
+import localization from 'moment/locale/pl'
+import 'react-big-calendar/lib/css/react-big-calendar.css';
+import '../styles/DoctorInfo.css'
 //import { db } from '../firebase';
+
+BigCalendar.momentLocalizer(moment);
+const events = [
+    {
+      'title': 'Zajęty termin',
+      'start': new Date(2018, 0, 4, 9, 0, 0),
+      'end': new Date(2018, 0, 4, 9, 30, 0)
+    },
+    {
+      'title': 'Long Event',
+      'start': new Date(2018, 0, 20),
+      'end': new Date(2018, 0, 20)
+    }
+]
+
+const minTime = new Date();
+    minTime.setHours(8,0,0);
+    const maxTime = new Date();
+    maxTime.setHours(18,1,0);
 
 class DoctorInfo extends Component {
     
@@ -7,31 +31,71 @@ class DoctorInfo extends Component {
         super(props);
     
         this.state = {
-          doctorID: null
+             messages:  {
+                date: 'Data',
+                time: 'Teraz',
+                event: 'Wizyta',
+                allDay: 'cały dzień',
+                next:">",
+                previous:"<",
+                today:"dzisiaj",
+                month: "miesiąc",
+                week: "tydzień",
+                day: "dzień",
+                agenda: "terminarz"
+            }
         };
       }
+componentWillReceiveProps() {
 
+}
 componentDidMount() {
-    var recievedMessage = this.props.location.state.doctorID
-    console.log(recievedMessage);
     // const { location } = this.props
 //  db.onceGetDoctors().then(snapshot =>
 //  this.setState(() => ({doctors: snapshot.val()} ))
 //   );
   }
+  eventStyleGetter(event, start, end, isSelected) {
+console.log(event);
+var backgroundColor = '#' + 'EFACAE';
+    var style = {
+        backgroundColor: backgroundColor,
+        borderRadius: '0px',
+        opacity: 0.8,
+        color: 'black',
+        border: '0px',
+    };
+    return {
+        style: style
+    };
+  }
     render() {
-        
-
-       
-    return (        
-<div>
+        return (        
+<div className="info__container">
  <p>Doctor Info {this.props.location.state.doctorId} 
  {this.props.location.state.doctorName} 
  {this.props.location.state.doctorRole} 
  {this.props.location.state.doctorAvatar}</p>
+ <BigCalendar
+        events={events}
+        messages={this.state.messages}
+        min={minTime}
+        max={maxTime}
+        // onNavigate={(date, view) => {
+        //     console.log('#### onNavigate');
+        //     console.log('#### date=', date);
+        //     console.log('#### view=', view);
+        //     //this.setState({currentDate: date});
+        //   }}
+        startAccessor='start'
+        endAccessor='end'
+       eventPropGetter={(this.eventStyleGetter)}
+      />
 </div>
 )
 }
 }
+
+
 
   export default DoctorInfo;
