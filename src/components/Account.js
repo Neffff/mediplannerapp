e37e37a64cdf2e1549a6aa44921d5e6cf3adf79e
Neffff <saymek@o2.pl>
2018-01-15 00:08:43 +0100

    account styles

--- src/components/Account.js
+++ src/components/Account.js
@@ -5,7 +5,10 @@ import PasswordChangeForm from './PasswordChange';
 import withAuthorization from './withAuthorization';
 import {db} from '../firebase';
 import Paper from 'material-ui/Paper';
+import Button from 'material-ui/Button';
+import Avatar from 'material-ui/Avatar';
 import moment from 'moment';
+import '../styles/Account.css'
 
 class AccountPage extends Component {
   constructor(props) {
@@ -16,7 +19,6 @@ class AccountPage extends Component {
       eventsUser: null,
       doctors: null,
     };
-// this.deleteEvent = this.deleteEvent.bind(this);
   }
   componentWillMount() {
     this.setState({
@@ -41,8 +43,6 @@ class AccountPage extends Component {
   }
 deleteEvent(key) {
   let showItem = this.state.eventsUser[key].doctor;
-  console.log(showItem);
-  console.log(key)
   db.onceDeleteEvent(this.state.currentUser, key, this.state.eventsUser[key].doctor);
   db.onceGetUserEvents(this.state.currentUser).then(snapshot =>
     this.setState({ eventsUser: snapshot.val() }))
@@ -50,39 +50,67 @@ deleteEvent(key) {
   render() {
     const { user, eventsUser, doctors } = this.state;
     return (
-      <div>
-        <Paper>
+      <div className="account__container">
+      <div className="account__top">
+        <Paper className="account__info">
           <h1>Konto użytkownika: {user && user.username}</h1>
           <p>Adres e-mail: {user && user.email}</p>
           <p>Numer telefonu: {user && user.phone}</p>
         </Paper>
-        <Paper>
-          <p>Twoje przyszłe wizyty:</p>
-          <ul>
-            {(eventsUser && doctors) && Object.keys(eventsUser).map((key) =>
-           (new Date(moment(eventsUser[key].start, 'YYYY-M-DD-H-m-s')) > new Date()) && (<li key={key} id="start">Od: {eventsUser[key].start} 
-             Do: {eventsUser[key].end} 
-             z: {doctors[eventsUser[key].doctor].name}<button onClick={this.deleteEvent.bind(this, key)}>odwołaj wizytę</button></li>))
-               }
-          </ul>
-          </Paper>
-          
-          <Paper>
-          <p>Twoje wizyty, które już się odbyły:</p>
-          <ul>
-          {(eventsUser && doctors) && Object.keys(eventsUser).map((key) =>
-           (new Date(moment(eventsUser[key].start, 'YYYY-M-DD-H-m-s')) < new Date()) && (<li key={key}>Od: {eventsUser[key].start} 
-             Do: {eventsUser[key].end} 
-             z: {doctors[eventsUser[key].doctor].name}</li>))
-               }
-               </ul>
-          </Paper>
-          <Paper>
+        <Paper className="account__password">
           <p>Zapomniałeś hasła?</p>
           <PasswordForgetForm/>
           <p>Chcesz zmienić hasło?</p>
           <PasswordChangeForm/>
         </Paper>
+        </div>
+        <Paper className="account__fevents">
+          <p>Twoje przyszłe wizyty:</p>
+          <table>
+          <tbody>
+          <tr>
+    <th>Wizyta z</th>
+    <th>Rozpoczęcie</th>
+    <th>Zakończenie</th>
+    <th></th>
+  </tr>
+ 
+            {(eventsUser && doctors) && Object.keys(eventsUser).map((key) =>
+           (new Date(moment(eventsUser[key].start, 'YYYY-M-DD-H-m-s')) > new Date()) && (<tr key={key}>
+         <td><Avatar
+           src={doctors[eventsUser[key].doctor].avatar}
+           className="account__avatar"
+         /><span className="account__name">{doctors[eventsUser[key].doctor].name}</span></td>
+        <td>{moment(eventsUser[key].start, 'YYYY-M-DD-H-m-s').format('DD MMMM YYYY, [godzina] H:mm')}</td>
+             <td>{moment(eventsUser[key].end, 'YYYY-M-DD-H-m-s').format('DD MMMM YYYY, [godzina] H:mm')}</td>
+             <td><Button onClick={this.deleteEvent.bind(this, key)}>odwołaj wizytę</Button></td></tr>))
+               }
+               </tbody>
+          </table>
+          </Paper>
+          <Paper className="account__pevents">
+          <p>Twoje wizyty, które już się odbyły:</p>
+          <table>
+          <tbody>
+          <tr>
+    <th>Wizyta z</th>
+    <th>Rozpoczecie</th>
+    <th>Zakończenie</th>
+  </tr>
+          {(eventsUser && doctors) && Object.keys(eventsUser).map((key) =>
+           (new Date(moment(eventsUser[key].start, 'YYYY-M-DD-H-m-s')) < new Date()) && (<tr key={key}>
+           <td><Avatar
+           src={doctors[eventsUser[key].doctor].avatar}
+           className="account__avatar"
+         /><span className="account__name">{doctors[eventsUser[key].doctor].name}</span></td>
+        <td>{moment(eventsUser[key].start, 'YYYY-M-DD-H-m-s').format('DD MMMM YYYY, [godzina] H:mm')}</td>
+             <td>{moment(eventsUser[key].end, 'YYYY-M-DD-H-m-s').format('DD MMMM YYYY, [godzina] H:mm')}</td>
+             </tr>))
+               }
+               </tbody>
+               </table>
+          </Paper>
+          
       </div>
     );
   }
